4618abeaea3cddf120a61d3e881e53a58d52b72a
JeyarasaThenujan <thenu782@gmail.com>
2025-04-24 11:15:58 +0530

    modify the inventory  management

--- client/src/components/inventory/InventoryManagementAll.jsx
+++ client/src/components/inventory/InventoryManagementAll.jsx
@@ -46,7 +46,7 @@ export default function InventoryManagementAll() {
       setLoading(true);
       const searchQuery = urlParams.toString();
       try {
-        const res = await fetch(`/api/inventories/search/get?${searchQuery}`);
+        const res = await fetch(`/api/inventories/search/get?${searchQuery}`);//set the serch api
         const data = await res.json();
         setInventories(data);
       } catch (error) {
