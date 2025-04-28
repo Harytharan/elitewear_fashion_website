1fbef8fe2311a2bd424543b351b43726db909ecd
JeyarasaThenujan <thenu782@gmail.com>
2025-04-28 09:21:44 +0530

    modify the inventory report

--- client/src/components/inventory/InventoryReport.jsx
+++ client/src/components/inventory/InventoryReport.jsx
@@ -48,7 +48,7 @@ export default function InventoryReport({ inventory }) {
     // logo.src = "../../assets/img/Logo14.png";
 
     doc.setFontSize(28).setFont("Mooli", "bold").setTextColor(169, 132, 109);
-    doc.text("FashioNexus", 80, 15);
+    doc.text("ELITEWEAR FASHION", 80, 15);
 
     doc.setFont("helvetica", "normal").setFontSize(20).setTextColor(0, 0, 0);
     doc.text("Inventory Details Report", 65, 25);
